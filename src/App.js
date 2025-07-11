44fae2aae26f290b314db39db22ad6b4a392da4e
tichchok <Yitzchokparkedison@gmail.com>
2025-07-11 22:54:19 +0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Welcome to Reshon</h1>
+      <p>Your Zichron Devarim tool is live 🎉</p>
+    </div>
+  );
+}
+
+export default App;
