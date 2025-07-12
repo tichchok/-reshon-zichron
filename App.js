1746dcbf4b070b20a04c710f6349c89614dfc1fa
tichchok <Yitzchokparkedison@gmail.com>
2025-07-12 12:05:27 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,15 +1,1 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import SmartOfferPreview from './components/SmartOfferPreview';
-import OfferView from './components/OfferView';
 
-export default function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<SmartOfferPreview />} />
-        <Route path="/offer/:id" element={<OfferView />} />
-      </Routes>
-    </Router>
-  );
-}
